f45df8fef7d94ea6ab2b157c62df24b12736f5d2
utkutopcuoglu <utkutopcuoglu@users.noreply.github.com>
2021-08-07 13:18:20 +0000

    deploy: 6febd11c61af6f4a8619fdf3c6ddea317381370f

--- assets/js/search.js
+++ assets/js/search.js
@@ -47,7 +47,7 @@ jtd.onReady = function(ready) {
 
 function initSearch() {
     var request = new XMLHttpRequest();
-    request.open('GET', '/first_personal_blog/assets/js/search-data.json', true);
+    request.open('GET', '/WhatIHaveLearned/assets/js/search-data.json', true);
   
     request.onload = function(){
       if (request.status >= 200 && request.status < 400) {
